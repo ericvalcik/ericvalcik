50ed0a3e9d4ab44f29c67fc33f73c33d82f03bfc
eric valcik <eric.valcik@gmail.com>
2024-12-17 01:08:18 +0100

    center on mobile

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,13 +8,22 @@ export default function ThreeDCardDemo() {
   return (
     <div className="w-screen h-screen flex flex-row items-center justify-center">
     <CardContainer>
-      <CardBody className="w-[481px] h-[302px] overflow-hidden rounded-[16px] shadow-md rotate-90 flip:rotate-0">
+      <CardBody className="flip:hidden h-[481px] w-[302px] overflow-hidden rounded-[16px] shadow-md">
+          <Image
+            src="/final-rotated.png"
+            height="481"
+            width="302"
+            className="object-right"
+            alt="vizitka rotated"
+          />
+      </CardBody>
+      <CardBody className="hidden flip:block w-[481px] h-[302px] overflow-hidden rounded-[16px] shadow-md">
           <Image
             src="/final.png"
             height="302"
             width="481"
             className="object-top"
-            alt="thumbnail"
+            alt="vizitka"
           />
       </CardBody>
     </CardContainer>
